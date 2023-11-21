a717751aea39babfde28ab8981e3ba702157388a
fwhf <huijie.wang3@jgdt.com>
2023-11-21 23:42:16 +0800

    添加计算器

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,21 +1,26 @@
-import * as $math from 'mathjs';
-
+import { create, all } from 'mathjs';
+const mathjs = create(all);
+mathjs.config({ number: 'BigNumber' });
+ 
 const math = {
   // ti + t2
   addTwo (t1, t2) {
-    return parseFloat($math.format($math.chain(t1 || 0).add($math.bignumber(t2 || 0)).done()));
+    return parseFloat(mathjs.format(mathjs.chain(t1 || 0).add(mathjs.bignumber(t2 || 0)).done()));
   },
   // ti - t2
   subtractTwo (t1, t2) {
-    return parseFloat($math.format($math.chain(t1 || 0).subtract($math.bignumber(t2 || 0)).done()));
+    return parseFloat(mathjs.format(mathjs.chain(t1 || 0).subtract(mathjs.bignumber(t2 || 0)).done()));
   },
   // ti X t2
   multiplyTwo (t1, t2) {
-    return parseFloat($math.format($math.chain(t1 || 0).multiply($math.bignumber(t2 || 0)).done()));
+    return parseFloat(mathjs.format(mathjs.chain(t1 || 0).multiply(mathjs.bignumber(t2 || 0)).done()));
   },
   // ti / t2
   divideTwo (t1, t2) {
-    return parseFloat($math.format($math.chain(t1 || 0).divide($math.bignumber(t2 || 0)).done()));
+    return parseFloat(mathjs.format(mathjs.chain(t1 || 0).divide(mathjs.bignumber(t2 || 0)).done()));
+  },
+  evaluate (str) {
+    return parseFloat(mathjs.evaluate(str) )
   }
 };
 
