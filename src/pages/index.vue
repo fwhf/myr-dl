72a6d8b6d4cde070b522345506ffd2d44a422a3e
fwhf <1026983432@qq.com>
2023-11-23 18:06:52 +0800

    添加天气

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,57 @@
 <template>
-    <el-calendar v-model="value" />
+    <el-calendar ref="calendar" v-model="value">
+        <template #header="{ date }">
+            <span>{{ date }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ tianqiValue }}</span>
+            <el-button-group>
+                <el-button size="small" @click="selectDate('prev-year')">
+                    上年
+                </el-button>
+                <el-button size="small" @click="selectDate('prev-month')">
+                    上月
+                </el-button>
+                <el-button size="small" @click="selectDate('today')">今日</el-button>
+                <el-button size="small" @click="selectDate('next-month')">
+                    下月
+                </el-button>
+                <el-button size="small" @click="selectDate('next-year')">
+                    下年
+                </el-button>
+            </el-button-group>
+        </template>
+    </el-calendar>
 </template>
 
 <script setup>
-import testData from "@/data/test.json";
-import { ref } from "vue";
+import { ref } from 'vue'
+
+const calendar = ref()
+const selectDate = (val) => {
+    if (!calendar.value) return
+    calendar.value.selectDate(val)
+}
 
 const value = ref(new Date())
+
+let xhr = null;
+let tianqiValue = ref()
+if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+} else if (window.ActiveXObject) {
+    xhr = new ActiveXObject()
+} else {
+    xhr = null;
+}
+if (xhr) {
+    xhr.open('GET', 'https://api.seniverse.com/v3/weather/now.json?key=Sr6cDEGhdgp628VN5&location=ip&language=zh-Hans&unit=c');
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            let res = JSON.parse(xhr.response)
+            tianqiValue.value = res.results[0].location.name + ': ' + res.results[0].now.text
+            xhr = null;
+        }
+    }
+    xhr.send();
+}
 </script>
   
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
